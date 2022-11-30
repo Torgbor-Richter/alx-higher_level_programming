7ca18eccb2b957b57be67d17674fa01dd7bce8e4
Torgbor-Richter <torgborrichter23@gmail.com>
2022-11-30 19:23:34 +0000

    multi

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,6 @@
-#!/bin/bash
-# Bash script that takes in an URL, sends a GET request to the URL and displays the body of the response
-curl -sL "$1"
+#!/usr/bin/node
+
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+console.log(line1 + '\n' + line2 + '\n' + line3);
