37e88942a112300e4ff8f1ebb43bf1c8fc436ddc
Torgbor-Richter <torgborrichter23@gmail.com>
2022-12-01 06:47:17 +0000

    arguement

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-let x = 0;
-process.argv.forEach((_val, _index) => {
-  x++;
-});
-if (x <= 2) {
+
+const numOfArgs = process.argv.length;
+
+if (numOfArgs === 2) {
   console.log('No argument');
-} else if (x === 3) {
+} else if (numOfArgs === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
