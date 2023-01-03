79369643bb573a46e304ca90fd402126ca547b62
Torgbor-Richter <torgborrichter23@gmail.com>
2023-01-03 13:59:07 -0800

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-"""script fetches https://alx-intranet.hbtn.io/status
+Fetches https://intranet.hbtn.io/status
 """
-
 if __name__ == "__main__":
     import urllib.request
-
-    url = "https://alx-intranet.hbtn.io/status"
-
-    with urllib.request.urlopen(url) as r:
-    content = r.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8')))
